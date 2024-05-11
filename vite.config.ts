ed21d3d1f22aa94bd53e07263d4d6a8bfa59055a
volodymyr@affogata.com <v.kopynets@enkonix.com>
2024-05-11 16:28:59 +0300

    temporary add dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ClosePlugin from './vite-plugin-close.ts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    watch: false
-  },
   plugins: [
-    ClosePlugin(),
     vue(),
     Vuetify({
       autoImport: true
