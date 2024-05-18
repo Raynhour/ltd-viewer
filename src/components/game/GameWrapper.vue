2b40e182f6bca45201e043c28d22d067c00330f5
volodymyr@affogata.com <v.kopynets@enkonix.com>
2024-05-18 14:37:24 +0300

    navigation fix on games

--- src/components/game/GameWrapper.vue
+++ src/components/game/GameWrapper.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="text-left">
     <Teleport to="#header">
-      <v-tabs align-tabs="center" bg-color="basil" grow class="game-tabs">
-        <v-tab :to="{ name: ROUTER_NAMES.GAME_RESULT, params: { id: route.params.id } }"
-          >Result</v-tab
-        >
-        <v-tab :to="{ name: ROUTER_NAMES.GAME_BUILD, params: { id: route.params.id } }"
-          >Builds</v-tab
-        >
+      <v-tabs :model-value="currentTab" align-tabs="center" bg-color="basil" grow class="game-tabs">
+        <v-tab :value="ROUTER_NAMES.GAME_RESULT" @click.prevent="goToResult">Result</v-tab>
+        <v-tab :value="ROUTER_NAMES.GAME_BUILD" @click.prevent="goToBuilds"> Builds</v-tab>
       </v-tabs>
     </Teleport>
     <router-view :data="data" />
@@ -15,16 +11,30 @@
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import type { Game } from '@/entities/game.type'
 
 import ROUTER_NAMES from '@/router/routerNames'
+import { computed } from 'vue'
 
 defineProps<{
   data: Game
 }>()
 
 const route = useRoute()
+const router = useRouter()
+
+function goToBuilds() {
+  router.replace({ name: ROUTER_NAMES.GAME_BUILD, params: { id: route.params.id } })
+}
+
+function goToResult() {
+  router.replace({ name: ROUTER_NAMES.GAME_RESULT, params: { id: route.params.id } })
+}
+
+const currentTab = computed(() => {
+  return route.name
+})
 </script>
 
 <style lang="scss">
